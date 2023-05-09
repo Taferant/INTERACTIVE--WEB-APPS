03b4f283fdeff4f50f3868d8fead77cf1238337d
taferant <t.mlimi@lab.clicklearning.org>
2023-05-09 20:34:22 +0200

    tt

--- IWA3/nwabisi.js
+++ IWA3/nwabisi.js
@@ -1,8 +1,8 @@
 // nwabisa.js
 
-private. firstname = "Nwabisa"
-private. surname = "Gabe"
-public. role = "CEO"
+private firstname = "Nwabisa"
+private surname = "Gabe"
+public role = "CEO"
 
 private display= firstname + " " + surname + (" + role + ")
 document.querySelector('#nwabisa').innerText = display

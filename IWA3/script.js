e4b2689c415cbdf90042c4ff66cd451ec321cc14
taferant <t.mlimi@lab.clicklearning.org>
2023-05-10 10:02:53 +0200

    last commit

--- IWA3/script.js
+++ IWA3/script.js
@@ -2,4 +2,4 @@ import* user from './nwabisi.js';
 import user from './johannes.js';
 import user from './alex.js';
 
-console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+console.log('Roles:', nwabisa.role, johanes.role, alex.role);

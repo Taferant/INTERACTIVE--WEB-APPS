9ed1e4c477391da26cbb06495ebdd2c1e15dc3c6
taferant <t.mlimi@lab.clicklearning.org>
2023-05-10 12:40:29 +0200

    first commit

---
+++ IWA3/scripts1.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import campany from './configaration.js';
+import year form './configaration.js';
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message;

93a7678f9e332b447a86b4c366354c77a20dc4f5
taferant <t.mlimi@lab.clicklearning.org>
2023-05-11 02:26:56 +0200

    b

--- IWA3/script.js
+++ IWA3/script.js
@@ -1,5 +1,3 @@
-import* as nwabisi from './nwabisi.js';
-import* as johannes from './johannes.js';
-import*as alex from './alex.js';
+import * as alex from './alex.js';
 
 console.log('Roles:', nwabisa.role, johanes.role, alex.role);

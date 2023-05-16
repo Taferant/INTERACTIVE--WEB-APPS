e6e173f3564ee1b58d2a6f8e226a7020dedb0926
taferant <t.mlimi@lab.clicklearning.org>
2023-05-16 10:22:40 +0200

    first comiit

--- IWA5/javascript.js
+++ IWA5/javascript.js
@@ -2,22 +2,31 @@ FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = '0'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if location = NAM
-shipping = 600 
-else shipping = 800
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries =35 * 2
-pens = 5 * 'NONE_SELECTED' 
+if (location = RSA) 
+{ shipping === 400 && currency === 'R' }
 
-shipping = null
-currency = $
+else if ( location = NAM)
+{
+ shipping = 600   
+}
+ 
+else
+ {
+    shipping = 800
+}
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirts = 150 * 'NONE_SELECTED'
+const batteries =35 * 2
+const pens = 5 * 'NONE_SELECTED' 
+
+let shipping = null
+let currency = '$'
+
+if (shoes + batteries + pens + shirts > 1000 &&   ) {
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
@@ -25,7 +34,8 @@ if (shoes + batteries + pens + shirts > 1000 &&  ) {
 	}
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+if ((shipping = 0) && (customers !== 1)) 
+{ console.log(WARNING) }
 
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
